0a393f0e542e43c3764d8d9d470dcf6b78bf3c5c
Ranganathan <sanranganathan@lm-maa-22004789.paypalcorp.com>
2018-08-09 16:28:43 +0530

    added constructor

--- WordGame.java
+++ WordGame.java
@@ -1,18 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WordGame {
 
+
+    int numOfLetter;
     Map<String, List<String>> possibleWords;
+    List<String> possibleSubString;
     int difficulty;
 
+    WordGame(int numOfLetter){
+        this.numOfLetter = numOfLetter;
+        possibleSubString = new ArrayList<>();
+    }
 
     public void generateMap(){
         //change the class variable "possibleWords"
+//        possibleWords = new HashMap<>();
+//        possibleWords.put("")
     }
 
     public String nextGuess(){
-        
+
     }
 
     public int commonLetters(String guess, String actual){
