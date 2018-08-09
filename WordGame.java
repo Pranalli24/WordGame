75630660056a33abaf23a988a5268417fe89c0e3
Ranganathan <sanranganathan@lm-maa-22004789.paypalcorp.com>
2018-08-09 16:19:36 +0530

    innnitial commit, template

---
+++ WordGame.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.Map;
+
+public class WordGame {
+
+    Map<String, List<String>> possibleWords;
+    int difficulty;
+
+
+    public void generateMap(){
+        //change the class variable "possibleWords"
+    }
+
+    public String nextGuess(){
+        
+    }
+
+    public int commonLetters(String guess, String actual){
+
+    }
+
+    public static void main(String[] args){
+        //loop till win here
+    }
+
+}
